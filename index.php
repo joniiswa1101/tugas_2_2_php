c028deace21b1cb8f0617d5cc8c2252d9050a00b
Joni Iswanto <joniiswa1101@gmail.com>
2019-10-05 11:34:22 +0700

    Tugas 2-2 PHP
    aritmatika 2

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$jual = 80000;
+$beli = 50000;
+
+echo "Jual = $jual<br>";
+echo "Beli = $beli<br>";
+echo "Keuntungan = ?<br>";
+
+$keuntungan = (($jual - $beli) / $beli) * 100 ;
+echo "Jawaban : $keuntungan%";
+
+
+ ?>
